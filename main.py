960d7c4a8942c3290228328f021d0bcaf3228029
nafiz-hossain <nhremon8181@gmail.com>
2021-11-07 19:12:33 +0600

    Updated logic, added score

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Display configurationss
+# Display configurations
 dis_width = 800
 dis_height = 600
 dis = pygame.display.set_mode((dis_width, dis_height))
@@ -18,17 +18,19 @@ pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
 
-snake_block = 20
+snake_block = 15  # Reduce the size of the snake block
 snake_speed = 20
 
 font_style = pygame.font.SysFont(None, 50)
 
 bg_img = pygame.image.load('background.png')
 food_img = pygame.image.load('food.png')
-#food_img = pygame.transform.scale(food_img,(1,1))
 
-# Set size of food image
-food_img = pygame.transform.scale(food_img, (snake_block+5, snake_block+5))
+# Set size of food image (bigger)
+food_img = pygame.transform.scale(food_img, (snake_block * 3, snake_block * 3))
+
+# Define padding
+padding = 50  # Adjust this value as needed
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
@@ -37,6 +39,10 @@ def message(msg, color):
 def snake(snake_block, snake_list):
     for segment in snake_list:
         pygame.draw.rect(dis, green, [segment[0], segment[1], snake_block, snake_block])
+
+def your_score(score):
+    value = font_style.render("Your Score: " + str(score), True, white)
+    dis.blit(value, [0, 0])
 
 def gameLoop():
     game_over = False
@@ -51,8 +57,10 @@ def gameLoop():
     snake_list = []
     length_of_snake = 1
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
+    foodx = round(random.randrange(padding, dis_width - padding - snake_block) / 15.0) * 15.0
+    foody = round(random.randrange(padding, dis_height - padding - snake_block) / 15.0) * 15.0
+
+    score = 0
 
     while not game_over:
 
@@ -72,22 +80,37 @@ def gameLoop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -snake_block
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = snake_block
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -snake_block
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = snake_block
+                x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+
+        # Boundary collision detection
+        if x1 >= dis_width - snake_block:
+            print("Right Boundary Collision Detected:", x1, y1)
             game_close = True
+        elif x1 < padding:
+            print("Left Boundary Collision Detected:", x1, y1)
+            game_close = True
+        elif y1 >= dis_height - snake_block:
+            print("Bottom Boundary Collision Detected:", x1, y1)
+            game_close = True
+        elif y1 < padding:
+            print("Top Boundary Collision Detected:", x1, y1)
+            game_close = True
+
+
+
         x1 += x1_change
         y1 += y1_change
         dis.blit(bg_img, (0, 0))
@@ -98,19 +121,23 @@ def gameLoop():
         snake_list.append(snake_head)
         if len(snake_list) > length_of_snake:
             del snake_list[0]
-
-        for x in snake_list[:-1]:
-            if x == snake_head:
+        for segment in snake_list[:-1]:
+            if segment[0] == x1 and segment[1] == y1:
+                print("Self-Collision Detected")
                 game_close = True
+                break
 
         snake(snake_block, snake_list)
+        your_score(length_of_snake - 1)
 
         pygame.display.update()
 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
+        # Check if snake's head touches the food
+        if foodx - snake_block <= x1 <= foodx + snake_block and foody - snake_block <= y1 <= foody + snake_block:
+            foodx = round(random.randrange(padding, dis_width - padding - snake_block) / 15.0) * 15.0
+            foody = round(random.randrange(padding, dis_height - padding - snake_block) / 15.0) * 15.0
             length_of_snake += 1
+            score += 10
 
         clock.tick(snake_speed)
 
