11047df681f6c30f9e36b918c6d779b0a3f22005
_nafiz <nafiz.hossain@tigerit.com>
2022-02-11 12:04:33 +0600

    Added snake basic property

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Display configurations
+# Display configurationss
 dis_width = 800
 dis_height = 600
 dis = pygame.display.set_mode((dis_width, dis_height))
