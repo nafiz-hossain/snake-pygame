3cdafc604355184055b50acba80c7bfd4d403eaa
nafizzzzzzzzzz <nhremon8181@gmail.com>
2023-05-09 22:19:44 +0600

    Added loop to fix immediately close window

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ import pygame
 pygame.init()
 display = pygame.display.set_mode((400,400))
 pygame.display.update()
+
+gameOver = False
+while not gameOver:
+    for event in pygame.event.get():
+        print(event)
+
+
+
+
 pygame.quit()
 quit()
