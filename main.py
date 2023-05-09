b55e604b5a55438f600c9a81d853fa4c5df45575
nafizzzzzzzzzz <nhremon8181@gmail.com>
2023-05-09 23:56:25 +0600

    Added title

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
 pygame.init()
 display = pygame.display.set_mode((400,400))
+pygame.display.set_caption('Snake game by nafiz')
+
 pygame.display.update()
 
 gameOver = False
 while not gameOver:
     for event in pygame.event.get():
         print(event)
-
-
-
-
+    
 pygame.quit()
 quit()
